a05a7c55c89813246acf5c8a9741a4d49d262a08
nomi <nomananwar07@gmail.com>
2019-04-04 12:22:54 +0500

    2nd commit

--- assignment.go
+++ assignment.go
@@ -1,12 +1,22 @@
 package golangexamples
+import	"github.com/ehteshamz/greetings"
 
-func ConccatSlice(sliceToConcat []byte) string {
+
+func ConcatSlice(sliceToConcat []byte) string {
 	var str string
 	for i := 0; i < len(sliceToConcat); i++ {
 		str+=string(sliceToConcat[i]) + string('-')
 	}
-
 	return str
 }
 
+func Encrypt(sliceToEncrypt []byte, ceaserCount int) {
+	for i := 0; i < len(sliceToEncrypt); i++ {
+		sliceToEncrypt[i] = sliceToEncrypt[i] + byte(ceaserCount-48)
+	}
+}
 
+
+func EZGreetings(name string) string{
+	return greetings.PrintGreetings(name)
+}
